e8e13c7202377c348195f051a1f9cac44ff84fb4
shaked <shakedownez@gmail.com>
2018-11-26 13:56:48 +0200

    Added click events

--- app/src/main/java/com/example/shaked/helloworld/MainActivity.java
+++ app/src/main/java/com/example/shaked/helloworld/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.shaked.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText editText; // = null
+    private TextView changingTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // We got these 2 lines automatically when we created the activity
@@ -18,14 +23,42 @@ public class MainActivity extends AppCompatActivity {
 
         // Get references to all the views
         // Which we would like to change dynamically
-        EditText editText = findViewById(R.id.editTextIn);
+        editText = findViewById(R.id.editTextIn);
         Button toastButton = findViewById(R.id.toastButton);
         Button logButton = findViewById(R.id.logButton);
         Button textButton = findViewById(R.id.textButton);
-        TextView changingTextView = findViewById(R.id.changedTextView);
+        changingTextView = findViewById(R.id.changedTextView);
+
+        toastButton.setOnClickListener(this);
+        logButton.setOnClickListener(this);
+        textButton.setOnClickListener(this);
 
 
     }
 
+    @Override
+    public void onClick(View jacobsView) {
+        int viewsId = jacobsView.getId();
+        String textFromEditText = editText.getText().toString();
 
+        if (viewsId == R.id.toastButton) {
+            // do code for toast
+            Toast.makeText(this, textFromEditText, Toast.LENGTH_SHORT).show();
+        } else if (viewsId == R.id.logButton) {
+            // You should do a log
+            Log.e("TAG", textFromEditText);
+        } else if (viewsId == R.id.textButton) {
+            // Let's change the text view
+
+            // setText() changes the text of the TextView
+            // Based on the string given to the method
+            changingTextView.setText(textFromEditText);
+
+
+        } else {
+            // default,
+            // If you reached here - you made a mistake
+
+        }
+    }
 }
